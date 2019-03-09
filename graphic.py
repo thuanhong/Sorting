b07b677930169cf312ccb03186075b9178a60b5b
HongThuan <guest@localhost.localdomain>
2019-03-09 08:47:22 -0500

    add

--- graphic.py
+++ graphic.py
@@ -1,58 +1,56 @@
 import pyglet
-from time import sleep
 from pyglet.window import mouse, key
 from sorting_deck import *
+import math
 
 
 class num:
     def __init__(self, value, posx, posy):
-        self.num = pyglet.text.Label(str(value), font_size = 20,x = posx,y = posy)
+        self.num = pyglet.text.Label(value, font_size = 20,x = posx,y = posy)
 
     def draw(self):
         self.num.draw()
 
 
-# class square:
-#     def __init__(self, x, y, img = None):
-#         self.x = x
-#         self.y = y
-#         if img != None:
-#             self.sprite = pyglet.sprite.Sprite(img)
-#             self.sprite.x = self.x
-#             self.sprite.y = self.y
-#
-#     def draw(self):
-#         self.sprite.draw()
-
-
 def take_data():
-    list_num = []
+
     f = open('data', 'r')  # take data to handle from data file
-    for line in f.read().split('\n')[:-1]:
+    list_data = f.read().split('\n')[:-1]
+
+    list_draw = []
+    for index, number in enumerate(list_data[0].split(' ')):
+        list_draw.append(num(number, 100*(index+1), 360))
+
+    list_redo = []
+    # list_undo = []
+    for line in list_data[1:]:
         temp = []
         for y in line.split(' '):
             temp.append(int(y))
-        list_num.append(temp)
+        list_redo.append(temp)
+        # list_undo.append([int(line[2]), int(line[0])])
     f.close()
-    list_draw = []
-    for index, number in enumerate(list_num[0]):
-        list_draw.append(num(number, 100*(index+1), 360))
-    return list_num, list_draw
+
+    return list_redo, list_draw
 
 class gameWindow(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.frame_rate = 1/60.0
 
-        self.list_num, self.list_draw = take_data()
+        self.list_redo, self.list_draw = take_data()
+        self.list_redo.insert(0, self.list_redo[0].reverse())
+        self.len = len(self.list_redo) - 1
+        self.cor_cur = self.list_redo[0]
+
         self.count = 0
+        self.wait = False
 
         self.sorted = pyglet.text.Label('List have been sorted',
                                         font_size = 40,x = 300,y = 200)
-        self.step = pyglet.text.Label('Step :' + '  /' + str(len(self.list_draw)),
+        self.step = pyglet.text.Label('Step :' + '   /' + str(len(self.list_draw)+1),
                                         font_size = 30,x = 1000,y = 600)
-        self.current = pyglet.text.Label(str(self.count+1),
-                                        font_size = 30,x = 1120,y = 600)
+        self.current = pyglet.text.Label('1', font_size = 30,x = 1120,y = 600)
 
         self.image = pyglet.image.load('image.png')
         back_tmp = pyglet.image.load('bg.jpg')
@@ -62,39 +60,45 @@ class gameWindow(pyglet.window.Window):
     def on_draw(self):
         self.clear()
         self.back_ground.draw()
-        # square(100*(self.y+1), 354, self.image).draw()
-        # square(100*(self.y+1+1), 354, self.image).draw()
+
         for x in self.list_draw:
             x.draw()
         self.step.draw()
         self.current.draw()
-        if self.count == len(self.list_draw) - 1:
+        if self.count == len(self.list_redo) - 1:
             self.sorted.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
-        if button == mouse.LEFT and self.count < len(self.list_draw):
+        if button == mouse.LEFT and self.count < self.len and not self.wait:
             self.count += 1
-            self.current.text = str(self.count)
-        elif button == mouse.RIGHT and self.count > 0:
-            self.count -= 1
-            self.current.text = str(self.count)
+    #     # elif button == mouse.RIGHT and self.count > 0 and not self.wait:
+    #     #     self.count -= 1
+    #
+    # def update_bubble(self, dt):
+    #     if self.cor_cur != self.list_redo[self.count]:
+    #         self.wait = True
+    #         index = self.list_redo[self.count][0]
+    #         temp = self.list_redo[self.count][1]
+    #         if self.list_draw[index].num.x != 100*(temp+1):
+    #             self.list_draw[index].num.x += 2
+    #             self.list_draw[temp].num.x -= 2
+    #         else:
+    #             self.cor_cur = self.list_redo[self.count]
+    #             self.wait = False
+    #             self.list_draw[index].num.x, self.list_draw[temp].num.x = self.list_draw[temp].num.x, self.list_draw[index].num.x
+    #             self.list_draw[index].num.text, self.list_draw[temp].num.text = self.list_draw[temp].num.text, self.list_draw[index].num.text
 
-    def update_bubble(self, dt):
-        for index, number in enumerate(self.list_num[self.count]):
-            if str(number) != self.list_draw[index].num.text:
-                temp = index+1
-                # while self.list_draw[index].num.x != 100*(temp+1):
-                #     self.list_draw[index].num.x -= -2
-                #     self.list_draw[temp].num.x += -2
-                #     break
-                # else:
-                #     self.list_draw[index].num.x, self.list_draw[temp].num.x = self.list_draw[temp].num.x, self.list_draw[index].num.x
-                #     self.list_draw[index].num.text, self.list_draw[temp].num.text = self.list_draw[temp].num.text, self.list_draw[index].num.text
-                # break
-                
+    def update_insert(self, dt):
+        if self.cor_cur != self.list_redo[self.count]:
+            self.wait = True
+            for x in self.list_redo[self.count][:-1]:
+                if self.list_draw[x].num.x != 100*(x+1+1):
+                    self.list_draw[x].num.x += 2
 
     def update(self, dt):
-        self.update_bubble(dt)
+        # self.current.text = str(self.count)
+        # self.update_bubble(dt)
+        self.update_insert(dt)
 
 if __name__ == '__main__':
     game = gameWindow(1280, 720, 'Simulator Sort')
--- sorting_deck.py
+++ sorting_deck.py
@@ -5,20 +5,24 @@ import argparse
 
 def bubbleSort(arr, file):
     n = len(arr)
+    file.write(' '.join(str(v) for v in arr) + '\n')
     for x in range(n-1, -1, -1):
         for y in range(x):
             if arr[y+1] < arr[y]:
                 arr[y+1], arr[y] = arr[y], arr[y+1]
-                file.write(' '.join(str(v) for v in arr) + '\n')
+                file.write('{} {}'.format(y, y+1) + '\n')
                 print(*arr)
 
 
-def insertSort(arr):
+def insertSort(arr, file):
+    file.write(' '.join(str(v) for v in arr) + '\n')
     for i in range(1, len(arr)):
         if arr[i] < max(arr[:i]):
             for j in range(i):
                 if arr[j] > arr[i]:
                     arr[i], arr[j] = arr[j], arr[i]
+                    file.write(str(j) + ' ')
+            file.write(str(i) + '\n')
             print(*arr)
 
 
@@ -72,7 +76,7 @@ def mergeSort(arr):
 
 def main(algor, arr, file):
     if algor == 'insert':
-        insertSort(arr)
+        insertSort(arr, file)
     elif algor == 'quick':
         quickSort(arr, 0, len(arr) - 1)
     elif algor == 'merge':
